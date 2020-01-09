2394a12217b37a34e7ea00426081a1514e04d179
lirf2018 <512164882@qq.com>
2020-01-09 17:22:48 +0800

    代码更新

--- src/main/java/com/yufan/utils/VerifySign.java
+++ src/main/java/com/yufan/utils/VerifySign.java
@@ -50,5 +50,40 @@ public class VerifySign {
 
         return false;
     }
+    /**
+     * 生成签名
+     *
+     * @param bean      json内容
+     * @param appsecret 密钥
+     * @return
+     */
+    public static boolean getSign(ReceiveJsonBean bean, String appsecret) {
+        //传过来的sign
+        try {
+            String getSign = bean.getSign();
+            MyMap map = new MyMap();
+            //系统参数
+            map.put("sid", bean.getSid());
+            map.put("appsecret", appsecret);
+            map.put("timestamp", bean.getTimestamp());
+
+            JSONObject json = bean.getData();
+            for (Object k : json.keySet()) {
+                Object v = json.get(k);
+                //只取data第一层数据
+//                if (null != v && !(v instanceof JSONArray) && v.toString().indexOf("{") == -1 && v.toString().indexOf("[") == -1) {
+                map.put(k.toString(), v);
+//                }
+            }
+            String sign = MD5.enCodeStandard(HelpCommon.getSign(map) + appsecret);
+            if (null != getSign && getSign.equals(sign)) {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
 
 }
